cebc572d8879f512c25bd2951d98a5a494129c7f
BoazYang1 <93645681+BoazYang1@users.noreply.github.com>
2022-01-13 11:32:58 +0800

    Add files via upload

---
+++ classify/prepare_data_task_pred.py
@@ -0,0 +1,55 @@
+import json
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--data', default='../datas/snli_data_dir/train-gen.txt', type=str)
+parser.add_argument('--output',default='../datas/snli_data_dir/train-expl.json', type=str)
+# parser.add_argument('--data', default='../datas/snli_data_dir/dev-gen.txt', type=str)
+# parser.add_argument('--output',default='../datas/snli_data_dir/dev-expl.json', type=str)
+# parser.add_argument('--data', default='../datas/snli_data_dir/test-gen.txt', type=str)
+# parser.add_argument('--output',default='../datas/snli_data_dir/test-expl.json', type=str)
+args = parser.parse_args()
+
+with open(args.data, 'r') as f:
+    data = []
+    d = {}
+    for line in f.readlines():
+        line = line.strip()
+        if not line == '':
+            items = line.split('\t')
+
+            idx = items[0]
+            gold_label = items[1]
+            x = items[-1]
+            premise = x[:x.index('Hypothesis :')][10:]
+
+            hypothesis = x[x.index('Hypothesis :') + 13: x.index('Explanation :')].strip()
+
+            expl = x[x.index('Explanation :'):][13:].strip()
+
+            d['premise'] = premise
+            d['hypothesis'] = hypothesis
+            d['gold_label'] = gold_label
+
+            if not 'expl' in d:
+                d['expl'] = {}
+
+            cur_label = ''
+            if len(d['expl']) == 0:
+                cur_label = 'entailment'
+            elif len(d['expl']) == 1:
+                cur_label = 'neutral'
+            elif len(d['expl']) == 2:
+                cur_label = 'contradiction'
+
+            d['expl'][cur_label] = expl
+
+            if len(d['expl']) == 3:
+                assert all([lb in d['expl'] for lb in ['entailment', 'neutral', 'contradiction']])
+                data.append(d)
+                d = {}
+
+with open(args.output, 'w') as fw:
+    for item in data:
+        fw.writelines(json.dumps(item) + '\n')
